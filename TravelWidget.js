4669297d2025b570e17d1348aae608ef1f72c0aa
Davi Mello <davimello1994@gmail.com>
2024-03-18 15:11:00 -0400

    Calculating distance to format correct API request

--- TravelWidget.js
+++ TravelWidget.js
@@ -2,18 +2,10 @@ async function getTrafficData() {
   const getHours = minutes => Math.floor(minutes / 60);
   const getMinutes = minutes => Math.round(minutes % 60);
 
-  const location = await getLocation();
-  const locationLatitude = location.latitude;
-  const locationLongitude = location.longitude;
-  const destinationLatitude = Keychain.get('destinationLatitude');
-  const destinationLongitude = Keychain.get('destinationLongitude');
-  const bingMapsKey = Keychain.get('travelWidget');
-
-  // Construct the API request URL
-  const url = `https://dev.virtualearth.net/REST/v1/Routes/Driving?wayPoint.1=${destinationLatitude},${destinationLongitude}&wayPoint.2=${locationLatitude},${locationLongitude}&du=mi&key=${bingMapsKey}`;
   if (shouldRefreshWidget()) {
     // Make the API request
     try {
+      const url = await getURLRequest();
       const request = new Request(url);
       const response = await request.loadJSON();
       if (!response) {
@@ -40,30 +32,30 @@ async function getTrafficData() {
 
       switch (trafficStatus) {
         case 'bad':
-          // trigger notification
           totalTimeText.textColor = Color.white();
-          trafficStack.backgroundColor = new Color('#F48477');
+          trafficStack.backgroundColor = new Color('#BA352A');
           trafficStack.addText('☠️');
           let badTrafficText = trafficStack.addText(`${trafficTime} ${trafficTime === 0 ? 'min' : 'mins'}`);
-          badTrafficText.font = Font.semiboldSystemFont(18);
+          badTrafficText.font = Font.semiboldSystemFont(12);
           widget.backgroundColor = new Color('#E94335');
           badTrafficText.textColor = Color.white();
           break;
         case 'medium':
           totalTimeText.textColor = new Color('#202124');
-          trafficStack.backgroundColor = new Color('#FEEFBD');
+          trafficStack.backgroundColor = new Color('#C99504');
           trafficStack.addText('⚠️');
           let mediumTrafficText = trafficStack.addText(`${trafficTime} ${trafficTime === 0 ? 'min' : 'mins'}`);
-          mediumTrafficText.font = Font.semiboldSystemFont(18);
+          mediumTrafficText.font = Font.semiboldSystemFont(12);
           widget.backgroundColor = new Color('#FBBB05');
           mediumTrafficText.textColor = new Color('#202124');
           break;
         default:
+          // trigger notification
           totalTimeText.textColor = Color.white();
-          trafficStack.backgroundColor = new Color('#4EDF89');
+          trafficStack.backgroundColor = new Color('#0C7D46');
           trafficStack.addText('✅');
           let goodTrafficText = trafficStack.addText(`${trafficTime} ${trafficTime === 0 ? 'min' : 'mins'}`);
-          goodTrafficText.font = Font.semiboldSystemFont(18);
+          goodTrafficText.font = Font.semiboldSystemFont(12);
           widget.backgroundColor =
             new Color('#0F9D58');
           goodTrafficText.textColor = Color.white();
@@ -110,11 +102,55 @@ function shouldRefreshWidget() {
   // Check if it's within the desired refresh times
   if ((hour >= 5 && hour < 8) || (hour >= 13 && hour < 19)) {
     // Check if it's the start of the hour
-    if (minute === 0) {
+    if (minute % 10 === 0) {
       return true;
     }
   }
   return false;
+}
+
+// Function to get distance between two coordinates in miles
+function getDistanceInMiles(lat1, lon1, lat2, lon2) {
+  // Convert degrees to radians
+  const toRadians = (degrees) => degrees * Math.PI / 180;
+
+  // Haversine formula
+  const R = 6371; // Radius of the Earth in kilometers
+  const dLat = toRadians(lat2 - lat1);
+  const dLon = toRadians(lon2 - lon1);
+  const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(toRadians(lat1)) * Math.cos(toRadians(lat2)) *
+    Math.sin(dLon / 2) * Math.sin(dLon / 2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  const distanceInKilometers = R * c;
+
+  // Convert kilometers to miles
+  const distanceInMiles = distanceInKilometers * 0.621371;
+
+  return distanceInMiles;
+}
+
+async function getURLRequest() {
+  const location = await getLocation();
+  const locationLatitude = location.latitude;
+  const locationLongitude = location.longitude;
+  const originLatitude = Keychain.get('originLatitude');
+  const originLongitude = Keychain.get('originLongitude');
+  const destinationLatitude = Keychain.get('destinationLatitude');
+  const destinationLongitude = Keychain.get('destinationLongitude');
+  const bingMapsKey = Keychain.get('travelWidget');
+
+  const distanceToOrigin = getDistanceInMiles(locationLatitude, locationLongitude, originLatitude, originLongitude);
+  const distanceToDestination = getDistanceInMiles(locationLatitude, locationLongitude, destinationLatitude, destinationLongitude);
+
+  // Construct the API request URL
+  let url = '';
+  if (distanceToOrigin > distanceToDestination) {
+    url = `https://dev.virtualearth.net/REST/v1/Routes/Driving?wayPoint.1=${locationLatitude},${locationLongitude}&wayPoint.2=${originLatitude},${originLongitude}&du=mi&key=${bingMapsKey}`;
+  } else {
+    url = `https://dev.virtualearth.net/REST/v1/Routes/Driving?wayPoint.1=${locationLatitude},${locationLongitude}&wayPoint.2=${destinationLatitude},${destinationLongitude}&du=mi&key=${bingMapsKey}`;
+  }
+  return url;
 }
 
 function getTrafficStatus(minutes) {
